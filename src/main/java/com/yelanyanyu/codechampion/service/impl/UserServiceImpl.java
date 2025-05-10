71613c1f6e5f6a072091c1ea3399451e595be3ea
yelanyanyu <yelanyanyu@outlook.com>
2025-05-10 20:18:06 +0800

    Refactor author information in multiple files; update author names and remove unnecessary links in comments

--- src/main/java/com/yelanyanyu/codechampion/service/impl/UserServiceImpl.java
+++ src/main/java/com/yelanyanyu/codechampion/service/impl/UserServiceImpl.java
@@ -72,6 +72,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             User user = new User();
             user.setUserAccount(userAccount);
             user.setUserPassword(encryptPassword);
+            // 默认用户昵称和账号相同
+            user.setUserName(userAccount);
             boolean saveResult = this.save(user);
             if (!saveResult) {
                 throw new BusinessException(ErrorCode.SYSTEM_ERROR, "注册失败，数据库错误");
